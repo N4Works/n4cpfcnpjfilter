7affb02cfe6ba1ed1dd0c1b0dc2babf9704fa13a
Tiago Resende <tiagor87@gmail.com>
2015-08-01 11:50:09 -0300

    Implementa filtro para CPF / CNPJ

---
+++ README.md
@@ -0,0 +1,11 @@
+# n4CpfCnpjFilter
+[![Build Status](https://secure.travis-ci.org/tiagor87/n4CpfCnpjFilter.png?branch=master)](https://travis-ci.org/tiagor87/n4CpfCnpjFilter)
+[![Coverage Status](https://coveralls.io/repos/tiagor87/n4CpfCnpjFilter/badge.svg?branch=master)](https://coveralls.io/r/tiagor87/n4CpfCnpjFilter/?branch=master)
+
+# about
+
+> n4CpfCnpjFilter: best AngularJS module ever!
+
+#LICENSE
+
+MIT
---
+++ tests/n4CpfCnpjFilter_test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+describe('CpfCnpjFilter', function()
+{
+  var _cpfCnpjFilter;
+
+  beforeEach(module('n4CpfCnpjFilter'));
+
+  beforeEach(inject(function($injector)
+  {
+    _cpfCnpjFilter = $injector.get('CpfCnpjFilter');
+  }));
+
+  describe('Methods', function()
+  {
+    it('should return an empty string when no value is specified', function()
+    {
+      expect(_cpfCnpjFilter()).toEqual('');
+    });
+
+    it('should return masked CNPJ when a CNPJ string is specified', function()
+    {
+        expect(_cpfCnpjFilter('11111111111111')).toEqual('11.111.111/1111-11');
+    });
+
+    it('should return masked CPF when a CPF string is specified', function()
+    {
+        expect(_cpfCnpjFilter('11111111111')).toEqual('111.111.111-11');
+    });
+
+    it('should return an empty string when an invalid string is specified', function()
+    {
+        expect(_cpfCnpjFilter('11111111')).toEqual('');
+    });
+  });
+});
