8dea1cd50559fe5a9ae59a9aa8358f149b78a5a3
Tiago Resende <tiagor87@gmail.com>
2015-08-03 19:44:23 -0300

    Gera arquivo não minificado

---
+++ dist/n4cpfcnpjfilter.js
@@ -0,0 +1,24 @@
+"use strict";
+
+;
+(function (ng) {
+  ng
+    .module('n4CpfCnpjFilter', [])
+    .filter('CpfCnpj', function () {
+      return function (cpfCnpj) {
+        if (!cpfCnpj) {
+          return '';
+        }
+
+        var digitos = cpfCnpj.replace(/\D/g, '');
+
+        if (digitos.length === 14) {
+          return digitos.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+        } else if (digitos.length === 11) {
+          return digitos.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+        } else {
+          return '';
+        }
+      };
+    });
+}(angular))
