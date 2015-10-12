f441bb926000453c7b5e0dffa76b4f890eebb92b
Tiago Resende <tiagor87@gmail.com>
2015-10-12 17:00:43 -0300

    Ajuste para minificação.

--- src/n4CpfCnpjFilter.js
+++ src/n4CpfCnpjFilter.js
@@ -1,24 +1,24 @@
-"use strict";
-
 ;
-(function (ng) {
+(function(ng) {
+  "use strict";
+
   ng
-    .module('n4CpfCnpjFilter', [])
-    .filter('CpfCnpj', function () {
-      return function (cpfCnpj) {
+    .module("n4CpfCnpjFilter", [])
+    .filter("CpfCnpj", function() {
+      return function(cpfCnpj) {
         if (!cpfCnpj) {
-          return '';
+          return "";
         }
 
-        var digitos = cpfCnpj.replace(/\D/g, '');
+        var digitos = cpfCnpj.replace(/\D/g, "");
 
         if (digitos.length === 14) {
-          return digitos.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+          return digitos.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
         } else if (digitos.length === 11) {
-          return digitos.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+          return digitos.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
         } else {
-          return '';
+          return "";
         }
       };
     });
-}(angular))
+}(angular));
